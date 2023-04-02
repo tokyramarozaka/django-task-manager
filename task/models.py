b97e75673ca899f5eff003b7fce4a38a7d56a35c
Tokimahery Ramarozaka <tokyramarozaka@gmail.com>
2023-04-02 16:43:52 +0300

    feat: added models and views

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=40)
+    category_priority = models.PositiveIntegerField(default=5)
+
+    def __str__(self) -> str:
+        return self.category_name
+
+class Task(models.Model):
+    task_title = models.CharField(max_length=150)
+    task_description = models.TextField(null=True, blank=True)
+    created_at = models.DateField(auto_now=True)
+    deadline = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.task_title
--- task/views.py
+++ task/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# TASKS
+def get_all_tasks(request):
+    return render(request, 'tasks.html', {})
+
+def get_task_by_id(request, id):
+    return render(request, 'single_task.html', {})
+
+def get_tasks_by_category(request, id):
+    return render(request, 'tasks.html', {})
+
+def post_task(request):
+    return render(request, 'tasks.html', {})
+
+# CATEGORY
+def get_all_categories(request):
+    return render(request, 'categories.html', {})
+
+def get_category_by_id(request, id):
+    return render(request, 'single_category.html', {})
+
+def post_category(request):
+    return render(request, 'categories.html', {})
